1cab875280add70321d08dd3f05041b931074549
leeseng0629 <tekken_teoh95@hotmail.com>
2018-08-26 17:23:51 +0800

    add new screen

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import {
 } from 'react-navigation';
 
 import BeginScreen from './BeginScreen';
+import ChooseModeScreen from './ChooseModeScreen'
 import ChoosePlayerScreen from './ChoosePlayerScreen';
 import GameScreen from './GameScreen';
 
 export default createStackNavigator({
   Begin: {
     screen: BeginScreen,
+  },
+  ChooseMode: {
+    screen: ChooseModeScreen
   },
   ChoosePlayer: {
     screen: ChoosePlayerScreen,
